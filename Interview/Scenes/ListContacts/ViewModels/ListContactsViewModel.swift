f43d43edae9b0ad0ab1e8a1fa926c6fbac749bd3
Julio Fernandes <julio.fernandes7@gmail.com>
2023-01-05 17:27:41 -0300

    criando um novo viewModel que utiliza o padrão de observable

--- Interview/Scenes/ListContacts/ViewModels/ListContactsViewModel.swift
+++ Interview/Scenes/ListContacts/ViewModels/ListContactsViewModel.swift
@@ -24,3 +24,27 @@ class ListContactsViewModel {
         }
     }
 }
+
+class NewListContactsViewModel {
+    
+    typealias Observer<T> = (T) -> Void
+    private let service: ListContactsLoader
+    init(service: ListContactsLoader) {
+        self.service = service
+    }
+    
+    var onLoadingChange: Observer<Bool>?
+    var onListContactsItems: Observer<[Contact]>?
+    
+    func loadService() {
+        onLoadingChange?(true)
+        service.load { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(items): self.onListContactsItems?(items)
+            default: break
+            }
+            self.onLoadingChange?(false)
+        }
+    }
+}
