dfdd2dd9ba0d5763918c488021194b8f7e442a1d
Julio Fernandes <julio.fernandes7@gmail.com>
2023-01-05 17:46:23 -0300

    nova ListViewController com regras apartadas de carregamento dos dados e refresh

--- Interview/Scenes/ListContacts/Views/ListContactsViewController.swift
+++ Interview/Scenes/ListContacts/Views/ListContactsViewController.swift
@@ -128,3 +128,30 @@ class ListContactsViewController: UIViewController, UITableViewDataSource, UITab
         }
     }
 }
+
+final class NewListContactsViewController: UITableViewController {
+    
+    var contacts = [ContactCellController]() {
+        didSet { tableView.reloadData() }
+    }
+    
+    private var refreshController: ListContactsRefreshController?
+    convenience init(refreshController: ListContactsRefreshController) {
+        self.init()
+        self.refreshController = refreshController
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        refreshControl = refreshController?.view
+        refreshController?.refresh()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return contacts[indexPath.row].renderCell()
+    }
+}
